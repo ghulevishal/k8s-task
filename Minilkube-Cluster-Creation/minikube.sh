8fede48f4c507e5f2be7d44cf2660a944a9d23be
Vishal Ghule <vishal@cloudyuga.guru>
2019-06-01 06:52:54 +0530

    Update minikube.sh

--- Minilkube-Cluster-Creation/minikube.sh
+++ Minilkube-Cluster-Creation/minikube.sh
@@ -9,7 +9,8 @@ apt-get update
 apt-get install -y kubectl socat
 }
 
-{curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+{
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
 }
 
 {
